cb4c14ff3dbd24667b6c464646ab9e589b415d3b
linChunbin <Ben@mwc-077136.dhcp.missouri.edu>
2018-03-18 13:02:46 -0500

    transfer to pi

--- arduino_test.c
+++ arduino_test.c
@@ -1,3 +1,5 @@
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include "rs232.h"
@@ -9,24 +11,24 @@ int main()
     int i=0;
     int cport_nr=25; /* /dev/ttyUSB0 */
     int bdrate=57600; /* 9600 baud */
-
+    
     char mode[]={'8','N','1',0}; // 8 data bits, no parity, 1 stop bit
-  //  char str_send[2][BUF_SIZE]; // send data buffer
+    char str_send[2][BUF_SIZE]; // send data buffer
     unsigned char str_recv[BUF_SIZE]; // recv data buffer
-//    strcpy(str_send[0], "This is a test string.");
-//    strcpy(str_send[1], "This is another test string.");
-//
+    strcpy(str_send[0], "This is a test string.");
+    strcpy(str_send[1], "This is another test string.");
+    
     if(RS232_OpenComport(cport_nr, bdrate, mode))
     {
         printf("Can not open comport\n");
         return(0);
     }
-
+    
     usleep(2000000);  /* waits 2000ms for stable condition */
     while(1)
     {
-//        RS232_cputs(cport_nr, str_send[i]); // sends string on serial
-//        printf("Sent to Arduino: '%s'\n", str_send[i]);
+        RS232_cputs(cport_nr, str_send[i]); // sends string on serial
+        printf("Sent to Arduino: '%s'\n", str_send[i]);
         usleep(1000000);  /* waits for reply 1000ms */
         int n = RS232_PollComport(cport_nr, str_recv, (int)BUF_SIZE);
         if(n > 0){
@@ -36,7 +38,7 @@ int main()
         i++;
         i %= 2;
         usleep(5000000);  /* sleep for 5 Second */
-
+        
     }
     return(0);
 }
