e0ee4597652152b9333349a3e6b6404905bed4a3
linChunbin <Ben@linChunbindeMacBook-Pro.local>
2018-04-29 18:01:15 -0500

    transferr

--- testSerial.c
+++ testSerial.c
@@ -15,9 +15,9 @@ int main()
     char mode[]={'8','N','1',0}; // 8 data bits, no parity, 1 stop bit
     char str_send[2][BUF_SIZE]; // send data buffer
     unsigned char str_recv[BUF_SIZE]; // recv data buffer
-    strcpy(str_send[0], "A");
-    strcpy(str_send[1], "B");
-
+   // strcpy(str_send[0], "A");
+   // strcpy(str_send[1], "B");
+    char a = 'A';
     if(RS232_OpenComport(cport_nr, bdrate, mode))
     {
         printf("Can not open comport\n");
@@ -27,7 +27,7 @@ int main()
     usleep(2000000);  /* waits 2000ms for stable condition */
     while(1)
     {
-        RS232_cputs(cport_nr, str_send[i]); // sends string on serial
+        RS232_cputs(cport_nr, A); // sends string on serial
         printf("Sent to Arduino: '%s'\n", str_send[i]);
         usleep(1000000);  /* waits for reply 1000ms */
         int n = RS232_PollComport(cport_nr, str_recv, (int)BUF_SIZE);
