e0776b8996367a38eafda191b099f07598690c36
ChunBin <clpk8@mail.missouri.edu>
2018-04-28 22:00:13 -0500

    transfer

--- arduino_test.c
+++ arduino_test.c
@@ -12,28 +12,28 @@ int main ()
     int fd ;
     int count ;
     unsigned int nextTime ;
-    
-    if ((fd = serialOpen ("/dev/ttyAMA0", 9600)) < 0)
+
+    if ((fd = serialOpen ("/dev/ttyACM0", 9600)) < 0)
     {
         fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
         return 1 ;
     }
-    
+
     if (wiringPiSetup () == -1)
     {
         fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
         return 1 ;
     }
     char data;
-    data = serialGetchar (handle)
-    while(1){
+    const char c = 'A';
+
         data = serialGetchar (fd);
         serialPutchar (fd, c) ;
 
-    }
-    
 
-    
+
+
+
 //    nextTime = millis () + 300 ;
 //
 //    for (count = 0 ; count < 256 ; )
@@ -55,7 +55,7 @@ int main ()
 //            fflush (stdout) ;
 //        }
 //    }
-    
+
     printf ("\n") ;
     return 0 ;
 }
