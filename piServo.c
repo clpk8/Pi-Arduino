2e362cca2ee43a0152c14dde0f4468fe978196c1
linChunbin <Ben@linChunbindeMacBook-Pro.local>
2018-05-01 09:50:44 -0500

    added a test

--- piServo.c
+++ piServo.c
@@ -9,8 +9,9 @@ int main (void)
     pwmSetMode (PWM_MODE_MS);
     pwmSetRange (2000);
     pwmSetClock (192);
-    pwmWrite(18,150);
+    pwmWrite(3,150);
+    
     delay(1000);
-    pwmWrite(18,200);
+    pwmWrite(3,200);
     return 0;
 }
